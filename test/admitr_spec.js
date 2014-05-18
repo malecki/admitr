58367fca29b6e344607a331167b326d1fd97f518
malecki <mike@crunch.io>
2014-05-18 00:19:17 -0400

    some more work on team groups, ensured that the fsms are hooked up to the directives in the page

--- test/admitr_spec.js
+++ test/admitr_spec.js
@@ -8,8 +8,8 @@ describe('Admitr app', function(){
     })
     describe('a Team', function(){
         beforeEach(function(){
-            inject(function(Team){
-                config = {
+            inject(function(Team, TeamGroup){
+                var config = {
                     name: 'A'
                     ,cap: 2
                     ,initialNumber: 0
@@ -26,6 +26,51 @@ describe('Admitr app', function(){
             sut.name.should.equal('A')
             sut.cap.should.equal(2)
             sut.state.should.equal('eligible')
+        })
+        it('should handle admissions and then be ineligible when full', function(){
+            var pt = {
+                name: 'Mr Jenkins'
+                ,service: ['Medicine']
+            }
+            sut.handle('admit', pt)
+            sut.handle('admit', pt)
+
+            sut.admitted.length.should.equal(2)
+            sut.admitted[0].name.should.equal('Mr Jenkins')
+            sut.state.should.equal('ineligible')
+        })
+    })
+    describe('A group of teams', function(){
+        beforeEach(function(){
+            inject(function(TeamGroup, Configurator){
+                var config = {name: 'Medicine', teams: [
+                    {name: 'A', cap: 10}
+                    ,{name: 'B', cap: 10}
+                ]}
+                sut = TeamGroup.create(config)
+            })
+        })
+        it('should have a service with teams A and B', function(){
+            sut.teams.length.should.equal(2)
+            var t = sut.teams.map(function(i){ return i.name })
+            t.should.eql(['A','B'])
+        })
+        it('should move a team upon admission to the bottom of the list', function(){
+
+        })
+        describe('Given a difference of < 4 patients', function(){
+            it('the order should be unchanged', function(){
+                sut.teams[0].initialNumber = 1
+                sut.handle('sort')
+                sut.teams[0].name.should.equal('A')
+            })
+        })
+        describe('Given a difference of ≥ 4 patients', function(){
+            it('should admit to the smaller team', function(){
+                sut.teams[0].initialNumber = 4
+                sut.handle('sort')
+                sut.teams[0].name.should.equal('B')
+            })
         })
     })
 })
